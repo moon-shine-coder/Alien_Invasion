76c7d7b8842f6d682ac1a6added2f76387fe1829
moon-shine-coder <semenvasilev09072008@gmail.com>
2025-05-09 13:49:52 +0300

    корабль может двигаться влево или вправо

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ class Settings():
     def __init__(self):
         '''Инициализирует настройки игры.'''
         # Параметры экрана
-        self.screen_width = 1200
-        self.screen_height = 800 
+        self.screen_width = 1080
+        self.screen_height = 960
         self.bg_color = (230, 230, 230)
+
+        # Настройка корабля 
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -6,12 +6,34 @@ class Ship():
         '''Инициализирует корабль и задаёт его начальную позицию.'''
         self.screen = ai_game.screen 
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
+
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
+        # Флаги перемещения
+        self.moving_right = False
+        self.moving_left = False 
+
+    def update(self):
+        '''Обновляет позицию кораблся с учётом флага.'''
+        # Обноволяется атрибут x, не rect
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
+
+
 
     def blitme(self):
         '''Рисует корабль в текущей позиции.'''
